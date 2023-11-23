c08a0681a44d1c19e0a2a1f37dd41ab753fcb3a2
kvardekkvar <kvardekkvar@yandex.ru>
2023-11-23 17:11:01 +0300

    Add spotlessplugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,11 @@ rootProject.name = "otus-java-course"
 include("hw01-gradle")
 include("hw02-collections")
 
+pluginManagement {
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.diffplug.spotless") version "6.22.0"
+
+}
 }
